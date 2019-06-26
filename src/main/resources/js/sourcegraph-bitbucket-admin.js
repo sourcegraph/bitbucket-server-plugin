864a0383958de00046099f0156867d7c79d7236d
Loïc Guychard <loic@sourcegraph.com>
2019-06-26 16:42:40 +0200

    Add an admin interface allowing to set the Sourcegraph URL

---
+++ src/main/resources/js/sourcegraph-bitbucket-admin.js
@@ -0,0 +1,27 @@
+AJS.toInit(() => {
+  const url = AJS.contextPath() + '/rest/sourcegraph-admin/1.0/'
+
+  // Fetch Sourcegraph URL value on page load.
+  AJS.$.ajax({
+    url,
+    dataType: 'json'
+  }).done(({ url }) => {
+    document.getElementById('url').value = url
+  })
+
+  // Update Sourcegraph URL when form is submitted.
+  AJS.$('#admin').submit(e => {
+    e.preventDefault()
+    const data = JSON.stringify({
+      url: document.getElementById('url').value
+    })
+    console.log(data)
+    AJS.$.ajax({
+      url: AJS.contextPath() + "/rest/sourcegraph-admin/1.0/",
+      type: "PUT",
+      contentType: "application/json",
+      data,
+      processData: false
+    });
+  })
+})
--- src/main/resources/js/sourcegraph-bitbucket.js
+++ src/main/resources/js/sourcegraph-bitbucket.js
@@ -1,12 +1,24 @@
 AJS.toInit(() => {
-  const sourcegraphURL = 'http://localhost:3080'
-  window.SOURCEGRAPH_URL = sourcegraphURL
-  window.localStorage.SOURCEGRAPH_URL = sourcegraphURL
-  window.SOURCEGRAPH_PHABRICATOR_EXTENSION = true;
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.defer = true;
-  script.src =
-    sourcegraphURL + '/.assets/extension/scripts/phabricator.bundle.js';
-  document.getElementsByTagName('head')[0].appendChild(script);
+    // Fetch Sourcegraph URL on page load
+    AJS.$.ajax({
+        url: AJS.contextPath() + '/rest/sourcegraph-admin/1.0/',
+        dataType: 'json'
+    }).done(({ url }) => {
+        if (!url) {
+            console.log('No Sourcegraph URL set')
+            return
+        }
+        console.log(url)
+        // If a Sourcegraph URL is set,
+        // inject a <script> tag to fetch the main JS bundle
+        // from the Sourcegraph instance
+        window.SOURCEGRAPH_URL = url
+        window.localStorage.SOURCEGRAPH_URL = url
+        window.SOURCEGRAPH_PHABRICATOR_EXTENSION = true
+        var script = document.createElement('script')
+        script.type = 'text/javascript'
+        script.defer = true
+        script.src = url + '/.assets/extension/scripts/integration.bundle.js'
+        document.getElementsByTagName('head')[0].appendChild(script)
+    })
 })
