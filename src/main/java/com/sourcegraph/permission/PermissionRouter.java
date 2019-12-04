b8a79992b5f9baa846812e9f6d8ffa4e2b7e7b65
Kevin Zheng <kevinzhenng@gmail.com>
2019-12-03 21:05:01 -0800

    Add getAccessibleRepositories endpoint

--- src/main/java/com/sourcegraph/permission/PermissionRouter.java
+++ src/main/java/com/sourcegraph/permission/PermissionRouter.java
@@ -2,8 +2,8 @@ package com.sourcegraph.permission;
 
 import com.atlassian.bitbucket.permission.Permission;
 import com.atlassian.bitbucket.permission.PermissionService;
-import com.atlassian.bitbucket.permission.PermittedGroup;
 import com.atlassian.bitbucket.repository.Repository;
+import com.atlassian.bitbucket.repository.RepositorySearchRequest;
 import com.atlassian.bitbucket.repository.RepositoryService;
 import com.atlassian.bitbucket.user.*;
 import com.atlassian.bitbucket.util.Page;
@@ -30,22 +30,19 @@ import java.io.IOException;
 @Component
 public class PermissionRouter {
     @ComponentImport
-    private static UserAdminService userAdminService;
-    @ComponentImport
     private static RepositoryService repositoryService;
     @ComponentImport
     private static UserManager userManager;
     @ComponentImport
-    private static PermissionService permissionService;
-    @ComponentImport
     private static UserService userService;
+    @ComponentImport
+    private static SecurityService securityService;
 
-    public PermissionRouter(UserAdminService users, RepositoryService repositoryService, UserManager userManager, PermissionService permissionService, UserService userService) {
-        PermissionRouter.userAdminService = users;
+    public PermissionRouter(RepositoryService repositoryService, UserManager userManager, UserService userService, SecurityService securityService) {
         PermissionRouter.repositoryService = repositoryService;
         PermissionRouter.userManager = userManager;
-        PermissionRouter.permissionService = permissionService;
         PermissionRouter.userService = userService;
+        PermissionRouter.securityService = securityService;
     }
 
     @GET
@@ -56,20 +53,13 @@ public class PermissionRouter {
             return Response.status(Response.Status.UNAUTHORIZED).build();
         }
 
-        String[] split = repository.split("/");
-        if (split.length < 2) {
-            return Response.status(Status.UNPROCESSABLE_ENTITY).build();
-        }
-
-        Repository repo = repositoryService.getBySlug(split[0], split[1]);
+        Repository repo = getRepository(repository);
         if (repo == null) {
-            return Response.status(Response.Status.NOT_FOUND).entity("No such project: " + repository).build();
+            return Response.status(Response.Status.NOT_FOUND).entity("No such repository: " + repository).build();
         }
 
-        Permission perm;
-        try {
-            perm = Permission.valueOf("REPO_" + permission.toUpperCase());
-        } catch (IllegalArgumentException e) {
+        Permission perm = getRepositoryPermission(permission);
+        if (perm == null) {
             return Response.status(Status.UNPROCESSABLE_ENTITY).build();
         }
 
@@ -85,10 +75,56 @@ public class PermissionRouter {
             System.out.println(page.getSize());
             for (ApplicationUser user : page.getValues()) {
                 System.out.println(user.getDisplayName());
-                bitmap.flip(user.getId());
+                bitmap.add(user.getId());
             }
             pageRequest = page.getNextPageRequest();
         } while (pageRequest != null);
+
+        byte[] backing;
+        try {
+            backing = serialize(bitmap);
+        } catch (IOException ex) {
+            return Response.serverError().build();
+        }
+        return Response.ok(backing).build();
+    }
+
+    @GET
+    @Path("/repositories")
+    public Response getAccessibleRepositories(@Context HttpServletRequest request, @QueryParam("user") String name, @QueryParam("permission") String permission) {
+        UserProfile profile = userManager.getRemoteUser(request);
+        if (profile == null || !userManager.isSystemAdmin(profile.getUserKey())) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+
+        ApplicationUser user = userService.getUserByName(name);
+        if (user == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity("No such user: " + user).build();
+        }
+
+        Permission perm = getRepositoryPermission(permission);
+        if (perm == null) {
+            return Response.status(Status.UNPROCESSABLE_ENTITY).build();
+        }
+
+        EscalatedSecurityContext context = securityService.impersonating(user, "Repository Search");
+        RoaringBitmap bitmap = context.call(() -> {
+            RoaringBitmap temp = new RoaringBitmap();
+
+            RepositorySearchRequest.Builder builder = new RepositorySearchRequest.Builder();
+            builder.permission(perm);
+            RepositorySearchRequest search = builder.build();
+
+            PageRequest pageRequest = new PageRequestImpl(0, 100);
+            do {
+                Page<Repository> page = repositoryService.search(search, pageRequest);
+                for (Repository repository : page.getValues()) {
+                    temp.add(repository.getId());
+                }
+                pageRequest = page.getNextPageRequest();
+            } while (pageRequest != null);
+            return temp;
+        });
 
         byte[] backing;
         try {
@@ -104,5 +140,19 @@ public class PermissionRouter {
         DataOutputStream out = new DataOutputStream(byteOut);
         bitmap.serialize(out);
         return byteOut.toByteArray();
+    }
+
+    public Repository getRepository(String name) {
+        String[] split = name.split("/");
+        return split.length < 2 ? null : repositoryService.getBySlug(split[0], split[1]);
+    }
+
+    public Permission getRepositoryPermission(String permission) {
+        Permission perm = null;
+        try {
+            perm = Permission.valueOf("REPO_" + permission.toUpperCase());
+        } catch (IllegalArgumentException ignored) {
+        }
+        return perm;
     }
 }
